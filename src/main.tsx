6b7551af94eee31b9acc20c4c06d7c72ee4842e0
민경호 <1736s@naver.com>
2023-09-09 23:14:47 +0900

    feat: 글로벌 스타일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
+import { GlobalStyle } from "./styles/GlobalStyle.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
