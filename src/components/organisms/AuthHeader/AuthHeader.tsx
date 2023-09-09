fd36e02a7142d44c63f96d2ed61117fb6bf8fe3b
민경호 <1736s@naver.com>
2023-09-10 01:00:58 +0900

    fix: 서비스 이름  변경

--- src/components/organisms/AuthHeader/AuthHeader.tsx
+++ src/components/organisms/AuthHeader/AuthHeader.tsx
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 import Text from "../../atoms/Text/Text";
-import theme from "../../../styles/theme";
 
 const AuthHeader = () => {
   return (
     <Header>
-      <Text label="Ppak Community" typo="bold1" color="black" />
+      <Text label="MS Community" typo="bold1" color="black" />
     </Header>
   );
 };
@@ -14,10 +13,8 @@ export default AuthHeader;
 
 const Header = styled.header`
   width: 100%;
-  padding: 20px 0;
+  padding: 100px 0 20px;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-bottom: 1px solid ${theme.color.gray4};
-  box-shadow: ${theme.shadow.normal};
 `;
