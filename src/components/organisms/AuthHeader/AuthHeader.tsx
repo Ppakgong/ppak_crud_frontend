cbc100fa7cb0d5cfe08390069bcbae70e9591c08
민경호 <1736s@naver.com>
2023-09-10 00:54:33 +0900

    feat: AuthHeader에 box-shadow와 border-bottom 추가

--- src/components/organisms/AuthHeader/AuthHeader.tsx
+++ src/components/organisms/AuthHeader/AuthHeader.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import Text from "../../atoms/Text/Text";
+import theme from "../../../styles/theme";
 
 const AuthHeader = () => {
   return (
     <Header>
-      <Text label="Ppak Community" typo="bold1" />
+      <Text label="Ppak Community" typo="bold1" color="black" />
     </Header>
   );
 };
@@ -13,8 +14,10 @@ export default AuthHeader;
 
 const Header = styled.header`
   width: 100%;
-  height: 100%;
+  padding: 20px 0;
   display: flex;
   justify-content: center;
   align-items: center;
+  border-bottom: 1px solid ${theme.color.gray4};
+  box-shadow: ${theme.shadow.normal};
 `;
