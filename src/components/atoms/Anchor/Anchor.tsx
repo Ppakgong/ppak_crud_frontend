bc636f59dabb66171d69122f9b0d376301793816
민경호 <1736s@naver.com>
2023-09-10 15:49:53 +0900

    fix: 앵커 컴포넌트에 propsWithChildren 추가

--- src/components/atoms/Anchor/Anchor.tsx
+++ src/components/atoms/Anchor/Anchor.tsx
@@ -1,44 +1,25 @@
 import { Link } from "react-router-dom";
-import styled, { css } from "styled-components";
-import theme, { ColorType, TypoType } from "../../../styles/theme";
+import styled from "styled-components";
+import { PropsWithChildren } from "react";
 
-export type AnchorStyleProps = {
-  $color?: ColorType;
-  $typo?: TypoType;
-  $gradient?: {
-    startColor: ColorType;
-    endColor: ColorType;
-  };
-};
-
-export type AnchorProps = AnchorStyleProps & {
+export type AnchorProps = {
   href: string;
-  label: string;
 };
 
-const Anchor = ({ href, label, ...props }: AnchorProps) => {
+const Anchor = ({
+  href,
+  children,
+  ...props
+}: PropsWithChildren<AnchorProps>) => {
   return (
     <Index to={href} {...props}>
-      {label}
+      {children}
     </Index>
   );
 };
 
 export default Anchor;
 
-const Index = styled(Link)<AnchorStyleProps>`
+const Index = styled(Link)`
   text-decoration: none;
-  color: ${({ $color = "gray1" }) => theme.color[$color]};
-  ${({ $typo = "medium4" }) => theme.typo[$typo]}
-  ${({ $gradient }) =>
-    $gradient &&
-    css`
-      background: linear-gradient(
-        to right,
-        ${theme.color[$gradient.startColor]} 11%,
-        ${theme.color[$gradient.endColor]} 100%
-      );
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    `}
 `;
