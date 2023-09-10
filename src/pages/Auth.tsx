0ceb488c995e3acea917a05ffac4dee3ce458839
이소윤 <soyun@isoyun-ui-MacBookAir.local>
2023-09-10 19:13:58 +0900

    Signup

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -27,8 +27,10 @@ const Container = styled.main`
   background-color: ${theme.color.blue_dark};
   width: 100%;
   height: 100%;
+  min-height: 100vh;
+  padding: 100px 0;
 `;
 
 const AuthHeaderWrapper = styled.div`
-  margin: 108px 0 48px;
+  margin-bottom: 48px;
 `;
--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -8,9 +8,12 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     html, body, #root {
-        width: 100%;
-        height: 100%;
         min-width: 100%;
         min-height: 100%;
+    }
+
+    body {
+        width:100%;
+        height: 100%;
     }
 `;
